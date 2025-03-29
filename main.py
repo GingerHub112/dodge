19ddeb21eb8dde4ad3ac812d66f06988ca8e4aef
GingerHub112 <163608082+GingerHub112@users.noreply.github.com>
2025-03-29 13:21:22 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
 import random
+from pygame import mixer
 
 pygame.init()
 pygame.font.init()
+mixer.init()
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 500, 500
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -13,6 +15,23 @@ colors = {
     "blue": (0, 0, 255),
     "red": (255, 0, 0)
 }
+
+bg = pygame.image.load("bg.png")
+bg = pygame.transform.scale(bg, (SCREEN_WIDTH, SCREEN_HEIGHT))
+player = pygame.image.load("player.png")
+player = pygame.transform.scale(player, (50, 50))
+brick = pygame.image.load("bricks.png")
+brick = pygame.transform.scale(brick, (50, 50))
+
+
+
+# Song: Tobu & Diviners - Geometry
+# Music provided by NoCopyrightSounds
+# Free Download/Stream: http://ncs.io/Geometry
+# Watch: http://ncs.lnk.to/GeometryAT/youtube
+mixer.music.load("ost.mp3")
+mixer.music.set_volume(0.5)
+mixer.music.play(-1)
 
 player_size = 50
 player_x = SCREEN_WIDTH // 2 - player_size // 2
@@ -26,7 +45,7 @@ obstacle_y = -obstacle_height
 obstacle_speed = 5
 
 font = pygame.font.Font('freesansbold.ttf', 32)
-text = font.render("Game Over!", True, pygame.Color("black"), colors["white"])
+text = font.render("Game Over!", True, pygame.Color("black"), None)
 textRect = text.get_rect()
 textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 
@@ -37,7 +56,9 @@ running = True
 clock = pygame.time.Clock()
 while running:
     screen.fill(colors["white"])
-    scoreText = font.render("Score: "+ str(score), True, pygame.Color("black"), colors["white"])
+    screen.blit(bg, (0, 0))
+
+    scoreText = font.render("Score: "+ str(score), True, pygame.Color("black"), None)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -65,8 +86,8 @@ while running:
         running = False
 
 
-    pygame.draw.rect(screen, colors["blue"], (player_x, player_y, player_size, player_size))
-    pygame.draw.rect(screen, colors["red"], (obstacle_x, obstacle_y, obstacle_width, obstacle_height))
+    screen.blit(player, (player_x, player_y))
+    screen.blit(brick, (obstacle_x, obstacle_y))
 
     screen.blit(scoreText, (0,0))
     pygame.display.update()
